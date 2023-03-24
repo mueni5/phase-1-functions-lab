8527f50a6c3c1082d6aa3f24bf751fe917b73c12
+ <+>
2023-03-24 08:24:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+let blockNumber = 43;
+blockNumber = 50;
+blockNumber = 34; 
+function distanceFromHqInBlocks(blockNumber) {
+    const hqBlock = 42;
+    return Math.abs(blockNumber - hqBlock);
+  }
+  
+  function distanceFromHqInFeet(blockNumber) {
+    const blockLength = 264;
+    return distanceFromHqInBlocks(blockNumber) * blockLength;
+  }
+
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    const blockLength = 264;
+    return Math.abs(startBlock - endBlock) * blockLength;
+  }
+
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+  }
+
+  
+  
